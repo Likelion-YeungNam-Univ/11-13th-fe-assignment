a654c27183547aa2f47f39054b36124844d675c7
Jihyun Kim <jihyun.q.s@gmail.com>
2025-05-26 02:36:27 +0900

    김정운  9회차 과제 불러오기

---
+++ 김정운/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div
+      className="flex flex-col justify-center items-center min-h-screen text-center text-3xl" src=""
+    >
+      <div className="flex-row text-center align-middle bg-white-400 rounded">
+        <p className="text-6xl">안녕하세요!</p>
+        <p className="pt-30"><strong>정운's Portfolio</strong> 입니다 😎</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
