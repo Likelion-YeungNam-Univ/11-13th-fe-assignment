879995378176bd4fa000d5a64a47d60247403e1a
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-30 17:55:39 +0900

    feat: 404 페이지 언어 전환 기능 추가

--- 신혜진/src/pages/NotFound.jsx
+++ 신혜진/src/pages/NotFound.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import LanguageContext from "../LanguageContext";
 
 const NotFound = () => {
   const navigate = useNavigate();
   const toHome = () => navigate("/");
+  const { language, changeLanguage } = useContext(LanguageContext);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-blue-50 text-center px-4">
-      <h1 className="text-8xl font-bold text-blue-950 mb-4">404</h1>
-      <h2 className="text-2xl font-semibold text-gray-600 mb-2">
-        페이지를 찾을 수 없습니다
-      </h2>
-      <p className="">
-        요청하신 페이지가 존재하지 않거나, 이동되었을 수 있습니다.
-      </p>
-      <button
-        onClick={toHome}
-        className="mt-5 px-6 py-3 bg-blue-950 hover:bg-white hover:text-blue-950 text-white rounded-xl shadow-md transition cursor-pointer font-bold"
-      >
-        홈으로 돌아가기
-      </button>
+      {language === "ko" ? (
+        <>
+          <h1 className="text-8xl font-bold text-blue-950 mb-4">404</h1>
+          <h2 className="text-2xl font-semibold text-gray-600 mb-2">
+            페이지를 찾을 수 없습니다
+          </h2>
+          <p className="">
+            요청하신 페이지가 존재하지 않거나, 이동되었을 수 있습니다.
+          </p>
+          <button
+            onClick={toHome}
+            className="mt-5 px-6 py-3 bg-blue-950 hover:bg-white hover:text-blue-950 text-white rounded-xl shadow-md transition cursor-pointer font-bold"
+          >
+            홈으로 돌아가기
+          </button>
+        </>
+      ) : (
+        <>
+          <h1 className="text-8xl font-bold text-blue-950 mb-4">404</h1>
+          <h2 className="text-2xl font-semibold text-gray-600 mb-2">
+            Page not found
+          </h2>
+          <p className="">
+            The page you requested may not exist or may have been moved.
+          </p>
+          <button
+            onClick={toHome}
+            className="mt-5 px-6 py-3 bg-blue-950 hover:bg-white hover:text-blue-950 text-white rounded-xl shadow-md transition cursor-pointer font-bold"
+          >
+            Return to Home
+          </button>
+        </>
+      )}
     </div>
   );
 };
