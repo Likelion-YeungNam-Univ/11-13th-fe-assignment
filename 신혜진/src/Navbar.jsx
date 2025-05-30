c7f840f2d3bcafde87d96b042f3c9a7dcb0e3447
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-30 17:23:55 +0900

    feat: Navbar - 언어 전환 버튼 추가

--- 신혜진/src/Navbar.jsx
+++ 신혜진/src/Navbar.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import LanguageContext from "./LanguageContext";
 
 const Navbar = () => {
   const linkStyle = "p-7 hover:text-white";
+  const { language, changeLanguage } = useContext(LanguageContext);
+
   return (
-    <nav className="p-4 bg-blue-950 text-gray-300 font-semibold">
-      <Link to="/" className={linkStyle}>
-        Home
-      </Link>
-      <Link to="/projects" className={linkStyle}>
-        Projects
-      </Link>
-      <Link to="/about" className={linkStyle}>
-        Introduction
-      </Link>
+    <nav className="flex justify-between p-4 bg-blue-950 text-gray-300 font-semibold">
+      <div>
+        <Link to="/" className={linkStyle}>
+          Home
+        </Link>
+        <Link to="/projects" className={linkStyle}>
+          Projects
+        </Link>
+        <Link to="/about" className={linkStyle}>
+          Introduction
+        </Link>
+      </div>
+      <button
+        onClick={changeLanguage}
+        className="border-2 px-2 bg-gray-300 text-blue-950 hover:bg-white cursor-pointer"
+      >
+        {language === "ko" ? "ENGLISH" : "한국어"}
+      </button>
     </nav>
   );
 };
