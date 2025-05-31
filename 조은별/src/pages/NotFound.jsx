feb708da84f28ca88d66543dc3bffa783811c9c3
eveveev <202104x@gmail.com>
2025-05-31 13:16:41 +0900

    업로드

--- 조은별/src/pages/NotFound.jsx
+++ 조은별/src/pages/NotFound.jsx
@@ -1,20 +1,37 @@
 import React from "react";
+import { useContext } from "react";
+import LanguageContext from "../LanguageContext";
 
 const NotFound = () => {
+  const { language } = useContext(LanguageContext);
+
+  const text = {
+    kor: {
+      title: "페이지를 찾을 수 없습니다.",
+      description: "요청하신 페이지가 존재하지 않거나, 이동되었을 수 있습니다.",
+      button: "홈으로 돌아가기",
+    },
+    eng: {
+      title: "Page Not Found",
+      description: "The page you are looking for is no longer available.",
+      button: "Return to Home",
+    },
+  };
+
+  const lan = text[language];
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br text-center px-5">
-      <h1 className="text-8xl font-extrabold text-green-500 mb-4">404</h1>
-      <h2 className="text-2xl md:text-3xl font-semibold text-green-800 mb-2">
-        페이지를 찾을 수 없습니다
+      <h1 className="text-8xl font-extrabold text-red-400 mb-4">404</h1>
+      <h2 className="text-2xl md:text-3xl font-semibold text-red-400 mb-2">
+        {lan.title}
       </h2>
-      <p className="text-black mb-6">
-        요청하신 페이지가 존재하지 않거나, 이동되었을 수 있습니다.
-      </p>
+      <p className="text-black mb-6">{lan.description}</p>
       <a
         href="/"
-        className="px-6 py-3 bg-green-600 text-white font-medium rounded-xl"
+        className="px-6 py-3 bg-red-400 text-white font-medium rounded-xl"
       >
-        홈으로 돌아가기
+        {lan.button}
       </a>
     </div>
   );
--- 조은별/src/pages/ProjectDetail.jsx
+++ 조은별/src/pages/ProjectDetail.jsx
@@ -1,38 +1,55 @@
 import { useParams, useNavigate } from "react-router-dom";
 import movieList from "../assets/movielist.png";
 import todoList from "../assets/todo.png";
+import { useContext } from "react";
+import LanguageContext from "../LanguageContext";
 
 const projectdata = {
   1: {
-    title: "To-Do List",
-    description: "할 일을 리스트에 추가하고 삭제할 수 있습니다.",
+    title: {
+      kor: "할 일 목록",
+      eng: "To-Do List",
+    },
+    description: {
+      kor: "할 일을 리스트에 추가하고 삭제할 수 있습니다.",
+      eng: "You can add and delete tasks from the list.",
+    },
     image: todoList,
   },
   2: {
-    title: "영화 목록",
-    description:
-      "영화 목록 중 시청한 영화, 보고 싶은 영화를 리스트에 담을 수 있습니다.",
+    title: {
+      kor: "영화 목록",
+      eng: "Movie List",
+    },
+    description: {
+      kor: "영화 목록 중 시청한 영화, 보고 싶은 영화를 리스트에 담을 수 있습니다.",
+      eng: "You can add movies you've watched or plan to watch to your movie list.",
+    },
     image: movieList,
   },
 };
+
 const ProjectDetail = () => {
   const { projectId } = useParams();
   const navigate = useNavigate();
   const project = projectdata[projectId];
+  const { language } = useContext(LanguageContext);
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-pink-50 px-4 py-12">
       <div className="bg-white rounded-2xl p-12 text-center">
         <h1 className="text-3xl font-bold text-red-400 mb-4">
-          {project.title}
+          {project.title[language]}
         </h1>
-        <p className="text-gray-700 mb-6">{project.description}</p>
+        <p className="text-gray-700 mb-6">{project.description[language]}</p>
         <img src={project.image} className="w-1/2 mb-4 rounded mx-auto" />
         <button
           onClick={() => navigate("/projects")}
-          className="p-2 bg-red-400 text-white rounded"
+          className="p-2 bg-red-400 text-white rounded cursor-pointer"
         >
-          프로젝트 목록으로 돌아가기
+          {language === "kor"
+            ? "프로젝트 목록으로 돌아가기"
+            : "Back to Project List"}
         </button>
       </div>
     </div>
